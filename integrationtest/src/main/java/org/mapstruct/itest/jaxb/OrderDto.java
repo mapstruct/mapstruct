6d2120c1bcb29fbeb34b6b1618d51f3579cb2250
sjaakd <sjaakderksen@zonnet.nl>
2014-02-26 21:20:24 +0100

    #119 setup jaxb XSDs, DTOs, mapper and test

---
+++ integrationtest/src/main/java/org/mapstruct/itest/jaxb/OrderDto.java
@@ -0,0 +1,58 @@
+/**
+ *  Copyright 2012-2014 Gunnar Morling (http://www.gunnarmorling.de/)
+ *  and/or other contributors as indicated by the @authors tag. See the
+ *  copyright.txt file in the distribution for a full listing of all
+ *  contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.mapstruct.itest.jaxb;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Sjaak Derksen
+ */
+public class OrderDto {
+
+    private Long orderNumber;
+    private Date orderDate;
+    private OrderDetailsDto orderDetails;
+
+
+    public Long getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber( Long orderNumber ) {
+        this.orderNumber = orderNumber;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate( Date orderDate ) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderDetailsDto getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails( OrderDetailsDto orderDetails ) {
+        this.orderDetails = orderDetails;
+    }
+}
---
+++ integrationtest/src/main/java/org/mapstruct/itest/jaxb/SourceTargetMapper.java
@@ -0,0 +1,46 @@
+/**
+ *  Copyright 2012-2014 Gunnar Morling (http://www.gunnarmorling.de/)
+ *  and/or other contributors as indicated by the @authors tag. See the
+ *  copyright.txt file in the distribution for a full listing of all
+ *  contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.mapstruct.itest.jaxb;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import org.mapstruct.itest.jaxb.xsd.test1.OrderDetailsType;
+import org.mapstruct.itest.jaxb.xsd.test1.OrderType;
+
+
+/**
+ *
+ * @author Sjaak Derksen
+ */
+@Mapper(uses = { org.mapstruct.itest.jaxb.xsd.test1.ObjectFactory.class,
+                 org.mapstruct.itest.jaxb.xsd.test2.ObjectFactory.class })
+public interface  SourceTargetMapper {
+
+    SourceTargetMapper INSTANCE = Mappers.getMapper( SourceTargetMapper.class );
+
+    // source 2 target methods
+    OrderDto sourceToTarget(OrderType source);
+    OrderDetailsDto detailsToDto(OrderDetailsType source);
+
+    // target 2 source methods
+    OrderType targetToSource(OrderDto target);
+    OrderDetailsType dtoToDetails(OrderDetailsDto target);
+
+}
