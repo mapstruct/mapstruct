c8f6be320448079b12dd8dcb44bf07755e0b1a9b
Andreas Gudian <agudian@apache.org>
2014-06-01 17:01:02 +0200

    #178 fix a new unused import that crept in, added a test for it

--- processor/src/test/java/org/mapstruct/ap/test/imports/SecondSourceTargetMapper.java
+++ processor/src/test/java/org/mapstruct/ap/test/imports/SecondSourceTargetMapper.java
@@ -20,6 +20,8 @@ package org.mapstruct.ap.test.imports;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.ap.test.imports.referenced.GenericMapper;
+import org.mapstruct.ap.test.imports.referenced.Source;
+import org.mapstruct.ap.test.imports.referenced.Target;
 import org.mapstruct.ap.test.imports.to.FooWrapper;
 import org.mapstruct.factory.Mappers;
 
@@ -32,4 +34,6 @@ public interface SecondSourceTargetMapper {
     SecondSourceTargetMapper INSTANCE = Mappers.getMapper( SecondSourceTargetMapper.class );
 
     FooWrapper fooWrapperToFooWrapper(org.mapstruct.ap.test.imports.from.FooWrapper foo);
+
+    Target sourceToTarget(Source source);
 }
