d85a691452ae03f38001978c8daf2710b9c023c5
sjaakd <sjaakderksen@zonnet.nl>
2014-06-23 14:43:44 +0200

    #233 moving conversion.generics package to selection.generics

---
+++ processor/src/test/java/org/mapstruct/ap/test/selection/jaxb/test1/OrderType.java
@@ -0,0 +1,73 @@
+/**
+ *  Copyright 2012-2014 Gunnar Morling (http://www.gunnarmorling.de/)
+ *  and/or other contributors as indicated by the @authors tag. See the
+ *  copyright.txt file in the distribution for a full listing of all
+ *  contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.mapstruct.ap.test.selection.jaxb.test1;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElementRef;
+import javax.xml.bind.annotation.XmlType;
+
+import org.mapstruct.ap.test.selection.jaxb.test2.OrderShippingDetailsType;
+
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "OrderType", propOrder = {
+    "orderNumber1",
+    "orderNumber2",
+    "shippingDetails"
+})
+public class OrderType {
+
+    @XmlElementRef(name = "orderNumber1", namespace = "http://www.mapstruct.org/ap/test/jaxb/selection/test1",
+        type = JAXBElement.class)
+    private JAXBElement<Long> orderNumber1;
+    @XmlElementRef(name = "orderNumber2", namespace = "http://www.mapstruct.org/ap/test/jaxb/selection/test1",
+        type = JAXBElement.class)
+    private JAXBElement<Long> orderNumber2;
+    @XmlElementRef(name = "shippingDetails", namespace = "http://www.mapstruct.org/ap/test/jaxb/selection/test1",
+        type = JAXBElement.class)
+    private JAXBElement<OrderShippingDetailsType> shippingDetails;
+
+    public JAXBElement<Long> getOrderNumber1() {
+        return orderNumber1;
+    }
+
+    public void setOrderNumber1(JAXBElement<Long> value) {
+        this.orderNumber1 = value;
+    }
+
+    public JAXBElement<Long> getOrderNumber2() {
+        return orderNumber2;
+    }
+
+    public void setOrderNumber2(JAXBElement<Long> value) {
+        this.orderNumber2 = value;
+    }
+
+    public JAXBElement<OrderShippingDetailsType> getShippingDetails() {
+        return shippingDetails;
+    }
+
+    public void setShippingDetails(JAXBElement<OrderShippingDetailsType> value) {
+        this.shippingDetails = value;
+    }
+
+}
---
+++ processor/src/test/java/org/mapstruct/ap/test/selection/jaxb/test2/ObjectFactory.java
@@ -0,0 +1,70 @@
+/**
+ *  Copyright 2012-2014 Gunnar Morling (http://www.gunnarmorling.de/)
+ *  and/or other contributors as indicated by the @authors tag. See the
+ *  copyright.txt file in the distribution for a full listing of all
+ *  contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.mapstruct.ap.test.selection.jaxb.test2;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+@XmlRegistry
+public class ObjectFactory {
+
+    public static final QName ORDER_SHIPPING_DETAILS_QNAME =
+        new QName( "http://www.mapstruct.org/ap/test/jaxb/selection/test2", "OrderShippingDetails" );
+    public static final QName ORDER_SHIPPING_DETAILS_TYPE_ORDER_SHIPPED_FROM_QNAME =
+        new QName( "http://www.mapstruct.org/ap/test/jaxb/selection/test2", "orderShippedFrom" );
+    public static final QName ORDER_SHIPPING_DETAILS_TYPE_ORDER_SHIPPED_TO_QNAME =
+        new QName( "http://www.mapstruct.org/ap/test/jaxb/selection/test2", "orderShippedTo" );
+
+    public ObjectFactory() {
+    }
+
+    public OrderShippingDetailsType createOrderShippingDetailsType() {
+        return new OrderShippingDetailsType();
+    }
+
+
+    @XmlElementDecl(namespace = "http://www.mapstruct.org/ap/test/jaxb/selection/test2", name = "OrderShippingDetails")
+    public JAXBElement<OrderShippingDetailsType> createOrderShippingDetails(OrderShippingDetailsType value) {
+        return new JAXBElement<OrderShippingDetailsType>(
+            ORDER_SHIPPING_DETAILS_QNAME,
+            OrderShippingDetailsType.class, null, value
+        );
+    }
+
+    @XmlElementDecl(namespace = "http://www.mapstruct.org/ap/test/jaxb/selection/test2", name = "orderShippedFrom",
+        scope = OrderShippingDetailsType.class)
+    public JAXBElement<String> createOrderShippingDetailsTypeOrderShippedFrom(String value) {
+        return new JAXBElement<String>(
+            ORDER_SHIPPING_DETAILS_TYPE_ORDER_SHIPPED_FROM_QNAME, String.class,
+            OrderShippingDetailsType.class, value
+        );
+    }
+
+    @XmlElementDecl(namespace = "http://www.mapstruct.org/ap/test/jaxb/selection/test2", name = "orderShippedTo",
+        scope = OrderShippingDetailsType.class)
+    public JAXBElement<String> createOrderShippingDetailsTypeOrderShippedTo(String value) {
+        return new JAXBElement<String>(
+            ORDER_SHIPPING_DETAILS_TYPE_ORDER_SHIPPED_TO_QNAME, String.class,
+            OrderShippingDetailsType.class, value
+        );
+    }
+
+}
