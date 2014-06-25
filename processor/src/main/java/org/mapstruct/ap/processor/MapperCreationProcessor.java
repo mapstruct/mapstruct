5f35f3f1e7696d966f2e41edebdc4653006f5819
sjaakd <sjaakderksen@zonnet.nl>
2014-06-25 20:19:58 +0200

    #242 fix for non collection source generates faulty code

--- processor/src/main/java/org/mapstruct/ap/processor/MapperCreationProcessor.java
+++ processor/src/main/java/org/mapstruct/ap/processor/MapperCreationProcessor.java
@@ -722,6 +722,7 @@ public class MapperCreationProcessor implements ModelElementProcessor<List<Sourc
         TargetAccessorType targetAccessorType;
         String sourceReference = parameter.getName() + "." + sourceAccessor.getSimpleName().toString() + "()";
         String iteratorReference = null;
+        boolean sourceIsCollection = false;
         if ( Executables.isSetterMethod( targetAccessor ) ) {
             sourceType = typeFactory.getReturnType( sourceAccessor );
             targetType = typeFactory.getSingleParameter( targetAccessor ).getType();
@@ -729,7 +730,8 @@ public class MapperCreationProcessor implements ModelElementProcessor<List<Sourc
         }
         else if ( Executables.isAdderMethod( targetAccessor ) ) {
             sourceType = typeFactory.getReturnType( sourceAccessor );
-            if ( sourceType.isCollectionType() && !sourceType.getTypeParameters().isEmpty() ) {
+            if ( sourceType.isCollectionType() ) {
+                sourceIsCollection = true;
                 sourceType = sourceType.getTypeParameters().get( 0 );
                 iteratorReference = Executables.getElementNameForAdder( targetAccessor );
             }
@@ -809,8 +811,16 @@ public class MapperCreationProcessor implements ModelElementProcessor<List<Sourc
                     }
                 }
                 else {
-                    // adder, so wrap as adder
-                    assignment = new AdderWrapper( assignment, method.getThrownTypes(), sourceReference, sourceType );
+                    // TargetAccessorType must be ADDER
+                    if ( sourceIsCollection ) {
+                        assignment =
+                                new AdderWrapper( assignment, method.getThrownTypes(), sourceReference, sourceType );
+                    }
+                    else {
+                        // Possibly adding null to a target collection. So should be surrounded by an null check.
+                        assignment = new SetterWrapper( assignment, method.getThrownTypes() );
+                        assignment = new NullCheckWrapper( assignment );
+                    }
                 }
             }
         }
