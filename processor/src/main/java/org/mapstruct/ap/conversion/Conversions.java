11068bfe1e1942c3f4607516054e1c0001cb876c
Timo E aus E <timo.eckhardt@gmail.com>
2014-07-16 20:06:52 +0200

    #121 support for converting java 8 dates and times to calendar and date

--- processor/src/main/java/org/mapstruct/ap/conversion/Conversions.java
+++ processor/src/main/java/org/mapstruct/ap/conversion/Conversions.java
@@ -18,13 +18,13 @@
  */
 package org.mapstruct.ap.conversion;
 
+import javax.lang.model.util.Elements;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import javax.lang.model.util.Elements;
 
 import org.mapstruct.ap.model.common.Type;
 import org.mapstruct.ap.model.common.TypeFactory;
@@ -181,6 +181,8 @@ public class Conversions {
 
         registerJodaConversions();
 
+        registerJava8TimeConversions();
+
         //misc.
         register( Enum.class, String.class, new EnumStringConversion() );
         register( Date.class, String.class, new DateToStringConversion() );
@@ -239,10 +241,54 @@ public class Conversions {
         );
     }
 
+    private void registerJava8TimeConversions() {
+        if ( !isJava8TimeAvailable() ) {
+            return;
+        }
+
+        // Java 8 time to String
+        register(
+                        getClass( JavaTimeConstants.ZONED_DATE_TIME_FQN ),
+                        String.class,
+                        new JavaZonedDateTimeToStringConversion()
+        );
+        register(
+                        getClass( JavaTimeConstants.LOCAL_DATE_FQN ),
+                        String.class,
+                        new JavaLocalDateToStringConversion()
+        );
+        register(
+                        getClass( JavaTimeConstants.LOCAL_DATE_TIME_FQN ),
+                        String.class,
+                        new JavaLocalDateTimeToStringConversion()
+        );
+        register(
+                        getClass( JavaTimeConstants.LOCAL_TIME_FQN ),
+                        String.class,
+                        new JavaLocalTimeToStringConversion()
+        );
+
+        // Java 8  to Date
+        register(
+                        getClass( JavaTimeConstants.ZONED_DATE_TIME_FQN ),
+                        Date.class,
+                        new JavaZonedDateTimeToDateConversion()
+        );
+
+        register(
+                        getClass( JavaTimeConstants.LOCAL_DATE_TIME_FQN ),
+                        Date.class,
+                        new JavaLocalDateTimeToDateConversion()
+        );
+
+    }
+
     private static boolean isJodaTimeAvailable() {
         return NativeTypes.isTypeAvailable( JodaTimeConstants.DATE_TIME_FQN );
     }
-
+    private static boolean isJava8TimeAvailable() {
+        return NativeTypes.isTypeAvailable( JavaTimeConstants.ZONED_DATE_TIME_FQN );
+    }
     private void registerNativeTypeConversion(Class<?> sourceType, Class<?> targetType) {
         if ( sourceType.isPrimitive() && targetType.isPrimitive() ) {
             register( sourceType, targetType, new PrimitiveToPrimitiveConversion( sourceType ) );
---
+++ processor/src/test/java/org/mapstruct/ap/test/conversion/java8time/SourceTargetMapper.java
@@ -0,0 +1,80 @@
+/**
+ *  Copyright 2012-2014 Gunnar Morling (http://www.gunnarmorling.de/)
+ *  and/or other contributors as indicated by the @authors tag. See the
+ *  copyright.txt file in the distribution for a full listing of all
+ *  contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.mapstruct.ap.test.conversion.java8time;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+/**
+ *
+ */
+@Mapper
+public interface SourceTargetMapper {
+
+    String DATE_TIME_FORMAT = "dd.MM.yyyy HH:mm z";
+
+    String LOCAL_DATE_TIME_FORMAT = "dd.MM.yyyy HH:mm";
+
+    String LOCAL_DATE_FORMAT = "dd.MM.yyyy";
+
+    String LOCAL_TIME_FORMAT = "HH:mm";
+
+    SourceTargetMapper INSTANCE = Mappers.getMapper( SourceTargetMapper.class );
+    @Mappings( { @Mapping( source = "zonedDateTime", dateFormat = DATE_TIME_FORMAT ),
+                    @Mapping( source = "localDateTime", dateFormat = LOCAL_DATE_TIME_FORMAT ),
+                    @Mapping( source = "localDate", dateFormat = LOCAL_DATE_FORMAT ),
+                    @Mapping( source = "localTime", dateFormat = LOCAL_TIME_FORMAT ) } )
+    Target sourceToTarget(Source source);
+
+    Target sourceToTargetDefaultMapping(Source source);
+
+    @Mapping( source = "zonedDateTime", dateFormat = DATE_TIME_FORMAT )
+    Target sourceToTargetDateTimeMapped(Source source);
+
+    @Mapping( source = "localDateTime", dateFormat = LOCAL_DATE_TIME_FORMAT )
+    Target sourceToTargetLocalDateTimeMapped(Source source);
+
+    @Mapping( source = "localDate", dateFormat = LOCAL_DATE_FORMAT )
+    Target sourceToTargetLocalDateMapped(Source source);
+
+    @Mapping( source = "localTime", dateFormat = LOCAL_TIME_FORMAT )
+    Target sourceToTargetLocalTimeMapped(Source source);
+
+    @Mappings( { @Mapping( source = "zonedDateTime", dateFormat = DATE_TIME_FORMAT ),
+                    @Mapping( source = "localDateTime", dateFormat = LOCAL_DATE_TIME_FORMAT ),
+                    @Mapping( source = "localDate", dateFormat = LOCAL_DATE_FORMAT ),
+                    @Mapping( source = "localTime", dateFormat = LOCAL_TIME_FORMAT ) } )
+    Source targetToSource(Target target);
+
+    @Mapping( source = "zonedDateTime", dateFormat = DATE_TIME_FORMAT )
+    Source targetToSourceDateTimeMapped(Target target);
+
+    @Mapping( source = "localDateTime", dateFormat = LOCAL_DATE_TIME_FORMAT )
+    Source targetToSourceLocalDateTimeMapped(Target target);
+
+    @Mapping( source = "localDate", dateFormat = LOCAL_DATE_FORMAT )
+    Source targetToSourceLocalDateMapped(Target target);
+
+    @Mapping( source = "localTime", dateFormat = LOCAL_TIME_FORMAT )
+    Source targetToSourceLocalTimeMapped(Target target);
+
+    Source targetToSourceDefaultMapping(Target target);
+}
