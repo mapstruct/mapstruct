b2ef82ba5c9c61cd2ace571f032f7197cefb9830
sjaakd <sjaakderksen@zonnet.nl>
2014-12-20 14:59:22 +0100

    #383 introduction of @InheritConfiguration to align update with create methods (single source)

---
+++ core-common/src/main/java/org/mapstruct/InheritConfiguration.java
@@ -0,0 +1,49 @@
+/**
+ *  Copyright 2012-2014 Gunnar Morling (http://www.gunnarmorling.de/)
+ *  and/or other contributors as indicated by the @authors tag. See the
+ *  copyright.txt file in the distribution for a full listing of all
+ *  contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.mapstruct;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Advises the code generator to apply all the {@link Mapping}s from a mapping method to the annotated method
+ * as well. The method should have the same source type and target type.
+ * <p>
+ * A typical use case is annotating an update method (a method with the {@link MappingTarget} in which an existing
+ * target is updated) in order to copy all defined mappings.
+ * <p>
+ * If more than one matching method exists, the name of the method to inherit the configuration from must be
+ * specified via {@link #name()}
+ *
+ * @author Sjaak Derksen
+ */
+@Target( ElementType.METHOD )
+@Retention( RetentionPolicy.SOURCE )
+public @interface InheritConfiguration {
+
+    /**
+     * The name of the mapping method to inherit the mappings from. Needs only to be specified in case more than
+     * one method with matching source and target type exists.
+     *
+     * @return The name of the mapping method to inherit the mappings from.
+     */
+    String name() default "";
+}
--- processor/src/test/java/org/mapstruct/ap/test/reverse/InheritInverseConfigurationTest.java
+++ processor/src/test/java/org/mapstruct/ap/test/reverse/InheritInverseConfigurationTest.java
@@ -120,18 +120,43 @@ public class InheritInverseConfigurationTest {
     }
 
     @Test
-    @WithClasses({ SourceTargetMapperErroneouslyAnnotated.class })
+    @WithClasses({ SourceTargetMapperErroneouslyAnnotated1.class })
     @ExpectedCompilationOutcome(
         value = CompilationResult.FAILED,
         diagnostics = {
-            @Diagnostic(type = SourceTargetMapperErroneouslyAnnotated.class,
+            @Diagnostic(type = SourceTargetMapperErroneouslyAnnotated1.class,
                 kind = Kind.ERROR,
-                line = 49,
+                line = 50,
                 messageRegExp = "Resolved inverse mapping method reverse\\(\\) should not carry the "
                     + "@InheritInverseConfiguration annotation itself.")
         }
     )
-    public void shouldUseWronglyAnnotatedError() {
+    public void shouldUseWronglyAnnotatedError1() {
+    }
+
+    @Test
+    @WithClasses({ SourceTargetMapperErroneouslyAnnotated2.class })
+    @ExpectedCompilationOutcome(
+        value = CompilationResult.FAILED,
+        diagnostics = {
+                @Diagnostic(type = SourceTargetMapperErroneouslyAnnotated2.class,
+                        kind = Kind.WARNING,
+                        line = 45,
+                        messageRegExp = "Unmapped target properties: \"stringPropX, integerPropX, "
+                                + "someConstantDownstream, propertyToIgnoreDownstream\""),
+                @Diagnostic(type = SourceTargetMapperErroneouslyAnnotated2.class,
+                        kind = Kind.ERROR,
+                        line = 47,
+                        messageRegExp = "Resolved inverse mapping method forward2\\(\\) should not carry "
+                                + "the @InheritConfiguration annotation"),
+                @Diagnostic(type = SourceTargetMapperErroneouslyAnnotated2.class,
+                        kind = Kind.WARNING,
+                        line = 48,
+                        messageRegExp = "Unmapped target properties: \"stringPropY, integerPropY, "
+                                + "propertyNotToIgnoreUpstream\"")
+            }
+    )
+    public void shouldUseWronglyAnnotatedError2() {
     }
 
     @Test
@@ -152,4 +177,5 @@ public class InheritInverseConfigurationTest {
     )
     public void shouldAdviseOnSpecifyingCorrectName() {
     }
+
 }
