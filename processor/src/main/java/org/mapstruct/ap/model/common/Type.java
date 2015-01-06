c2c2048605528b83f1583271b0a250296c35d132
Gunnar Morling <gunnar.morling@googlemail.com>
2015-01-06 23:05:08 +0100

    #213 Updating license header

--- processor/src/main/java/org/mapstruct/ap/model/common/Type.java
+++ processor/src/main/java/org/mapstruct/ap/model/common/Type.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2012-2014 Gunnar Morling (http://www.gunnarmorling.de/)
+ *  Copyright 2012-2015 Gunnar Morling (http://www.gunnarmorling.de/)
  *  and/or other contributors as indicated by the @authors tag. See the
  *  copyright.txt file in the distribution for a full listing of all
  *  contributors.
