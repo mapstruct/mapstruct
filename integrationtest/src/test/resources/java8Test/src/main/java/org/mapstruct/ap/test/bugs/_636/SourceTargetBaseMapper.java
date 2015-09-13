18532ace8c70eb97dc8ee39240c46abd7b1ff2c0
sjaakd <sjaakderksen@zonnet.nl>
2015-09-13 23:22:01 +0200

    #636 - adapt integration test for default and static interface methods (Java8) to eclipse compiler problem

--- integrationtest/src/test/resources/java8Test/src/main/java/org/mapstruct/ap/test/bugs/_636/SourceTargetBaseMapper.java
+++ integrationtest/src/test/resources/java8Test/src/main/java/org/mapstruct/ap/test/bugs/_636/SourceTargetBaseMapper.java
@@ -23,12 +23,6 @@ import org.mapstruct.Mapper;
 @Mapper
 public interface SourceTargetBaseMapper {
 
+    // TODO.. move default and static interface method here when problem in eclipse processor is fixed.
 
-    default Foo fooFromId(long id) {
-        return new Foo(id);
-    }
-
-    static Bar barFromId(String id) {
-        return new Bar(id);
-    }
 }
