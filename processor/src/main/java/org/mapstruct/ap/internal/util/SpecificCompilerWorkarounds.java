c870d23df691f549e649859e5ac673c8463c4394
Andreas Gudian <agudian@apache.org>
2015-11-06 23:40:25 +0100

    #644 Rough workaround for problems with the Eclipse implementation of asMemberOf

--- processor/src/main/java/org/mapstruct/ap/internal/util/SpecificCompilerWorkarounds.java
+++ processor/src/main/java/org/mapstruct/ap/internal/util/SpecificCompilerWorkarounds.java
@@ -18,11 +18,31 @@
  */
 package org.mapstruct.ap.internal.util;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
+
+import org.eclipse.jdt.core.compiler.CharOperation;
+import org.eclipse.jdt.internal.compiler.apt.dispatch.BaseProcessingEnvImpl;
+import org.eclipse.jdt.internal.compiler.apt.model.DeclaredTypeImpl;
+import org.eclipse.jdt.internal.compiler.apt.model.ElementImpl;
+import org.eclipse.jdt.internal.compiler.apt.model.TypeMirrorImpl;
+import org.eclipse.jdt.internal.compiler.apt.model.TypesImpl;
+import org.eclipse.jdt.internal.compiler.lookup.MethodBinding;
+import org.eclipse.jdt.internal.compiler.lookup.ReferenceBinding;
+import org.eclipse.jdt.internal.compiler.lookup.TypeBinding;
 
 /**
  * Contains workarounds for various quirks in specific compilers.
@@ -114,5 +134,162 @@ public class SpecificCompilerWorkarounds {
             }
         }
         return element;
+    }
+
+    public static TypeMirror asMemberOf(DeclaredType containing, Element element, Types typeUtils,
+                                        Elements elementUtils) {
+        try {
+            // quick and dirty, until implementation below is more sound
+            return typeUtils.asMemberOf( containing, element );
+        }
+        catch ( IllegalArgumentException e ) {
+            TypeMirror eclipseWorkaround =
+                EclipseEnvironment.asMemberOf( containing, element, typeUtils, elementUtils );
+
+            if ( null != eclipseWorkaround ) {
+                return eclipseWorkaround;
+            }
+
+            throw e;
+        }
+    }
+
+    private static final class EclipseEnvironment {
+        public static TypeMirror asMemberOf(DeclaredType containing, Element element, Types typeUtils,
+                                            Elements elementUtils) {
+            if ( typeUtils instanceof TypesImpl && containing instanceof DeclaredTypeImpl
+                && element instanceof ElementImpl ) {
+                try {
+                    ElementImpl elementImpl = (ElementImpl) element;
+
+                    Field field = TypesImpl.class.getDeclaredField( "_env" );
+                    field.setAccessible( true );
+                    BaseProcessingEnvImpl env = (BaseProcessingEnvImpl) field.get( typeUtils );
+
+                    // TODO can some of the reflection here be avoided?
+                    Method bindingGetter = TypeMirrorImpl.class.getDeclaredMethod( "binding" );
+                    bindingGetter.setAccessible( true );
+                    ReferenceBinding referenceBinding = (ReferenceBinding) bindingGetter.invoke( containing );
+
+                    MethodBinding methodBinding = (MethodBinding) elementImpl._binding;
+                    // matches in super-classes have priority
+                    MethodBinding inSuperclassHiearchy =
+                        findInSuperclassHierarchy( methodBinding, env, referenceBinding );
+                    if ( inSuperclassHiearchy != null ) {
+                        return env.getFactory().newTypeMirror( inSuperclassHiearchy );
+                    }
+
+                    // if nothing was found, traverse the interfaces and collect all candidate methods that match
+                    List<MethodBinding> candidatesFromInterfaces = new ArrayList<MethodBinding>();
+                    collectFromInterfaces(
+                        methodBinding,
+                        referenceBinding,
+                        new HashSet<ReferenceBinding>(),
+                        candidatesFromInterfaces );
+
+                    java.util.Collections.sort( candidatesFromInterfaces, new Comparator<MethodBinding>() {
+                        @Override
+                        public int compare(MethodBinding first, MethodBinding second) {
+                            boolean firstParamsAssignableFromSecond =
+                                first.areParametersCompatibleWith( second.parameters );
+                            boolean secondParamsAssignableFromFirst =
+                                second.areParametersCompatibleWith( first.parameters );
+
+                            if ( firstParamsAssignableFromSecond != secondParamsAssignableFromFirst ) {
+                                return firstParamsAssignableFromSecond ? 1 : -1;
+                            }
+
+                            if ( TypeBinding.equalsEquals( first.returnType, second.returnType ) ) {
+                                return 0;
+                            }
+
+                            boolean firstReturnTypeAssignableFromSecond =
+                                second.returnType.isCompatibleWith( first.returnType );
+
+                            return firstReturnTypeAssignableFromSecond ? 1 : -1;
+                        }
+                    } );
+
+                    if ( !candidatesFromInterfaces.isEmpty() ) {
+                        return env.getFactory().newTypeMirror( candidatesFromInterfaces.get( 0 ) );
+                    }
+                }
+                catch ( Exception e2 ) {
+                    throw new RuntimeException( "Fallback implementation of asMemberOf didn't work for "
+                        + element + " in " + containing, e2 );
+                }
+            }
+
+            return null;
+        }
+
+        /**
+         * @param methodBinding
+         * @param typeBinding
+         * @param found
+         * @return
+         */
+        private static void collectFromInterfaces(MethodBinding methodBinding, ReferenceBinding typeBinding,
+                                                  Set<ReferenceBinding> visitedTypes, List<MethodBinding> found) {
+            if ( typeBinding == null ) {
+                return;
+            }
+
+            // also check the interfaces of the superclass hierarchy (the superclasses themselves don't contain a match,
+            // we
+            // checked that already)
+            collectFromInterfaces( methodBinding, typeBinding.superclass(), visitedTypes, found );
+
+            for ( ReferenceBinding ifc : typeBinding.superInterfaces() ) {
+                if ( visitedTypes.contains( ifc ) ) {
+                    continue;
+                }
+
+                visitedTypes.add( ifc );
+
+                // finding a match in one interface
+                MethodBinding f = findMatchingMethodBinding( methodBinding, ifc.methods() );
+
+                if ( f == null ) {
+                    collectFromInterfaces( methodBinding, ifc, visitedTypes, found );
+                }
+                else {
+                    // no need for recursion if we found a candidate in this type already
+                    found.add( f );
+                }
+            }
+        }
+
+        /**
+         * @param baseMethod binding to compare against
+         * @param methods the candidate methods
+         * @return The method from the list of candidates that matches the name and original/erasure of
+         *         {@code methodBinding}, or {@code null} if none was found.
+         */
+        private static MethodBinding findMatchingMethodBinding(MethodBinding baseMethod, MethodBinding[] methods) {
+            for ( MethodBinding method : methods ) {
+                if ( CharOperation.equals( method.selector, baseMethod.selector )
+                    && ( method.original() == baseMethod || method.areParameterErasuresEqual( baseMethod ) ) ) {
+                    return method;
+                }
+            }
+
+            return null;
+        }
+
+        private static MethodBinding findInSuperclassHierarchy(MethodBinding baseMethod, BaseProcessingEnvImpl env,
+                                                               ReferenceBinding typeBinding) {
+            while ( typeBinding != null ) {
+
+                MethodBinding matching = findMatchingMethodBinding( baseMethod, typeBinding.methods() );
+                if ( matching != null ) {
+                    return matching;
+                }
+
+                typeBinding = typeBinding.superclass();
+            }
+
+            return null;
+        }
     }
 }
