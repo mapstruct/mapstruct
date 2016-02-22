73d39c8a6859431f068b14d53be09ee256a8e78a
sjaakd <sjaakderksen@zonnet.nl>
2016-02-23 00:43:59 +0100

    #749 Adding  javadoc example usages

--- core-jdk8/src/main/java/org/mapstruct/ValueMapping.java
+++ core-jdk8/src/main/java/org/mapstruct/ValueMapping.java
@@ -27,6 +27,142 @@ import java.lang.annotation.Target;
 /**
  * Configures the mapping of source constant value to target constant value.
  *
+ * Assume the following enumerations
+ * <pre>
+ * <code>
+ * public enum OrderType {
+ *
+ *   RETAIL, B2B, EXTRA, STANDARD, NORMAL
+ * }
+ * </code>
+ * </pre>
+ *
+ * <pre>
+ * <code>
+ * public enum ExternalOrderType {
+ *
+ *   RETAIL, B2B, SPECIAL, DEFAULT
+ * }
+ * </code>
+ * </pre>
+ *
+ * MapStruct can complete mappings based on names. Just handle the exceptions and let MapStruct do the remainder.
+ *
+ * So:
+ * <pre>
+ * <code>
+ * &#64;ValueMappings({
+ *     &#64;ValueMapping(source = "EXTRA", target = "SPECIAL"),
+ *     &#64;ValueMapping(source = "STANDARD", target = "DEFAULT"),
+ *     &#64;ValueMapping(source = "NORMAL", target = "DEFAULT") })
+ * ExternalOrderType orderTypeToExternalOrderType(OrderType orderType);
+ * </code>
+ * </pre>
+ *
+ * Will result in:
+ *
+ * <pre>
+ * <code>
+ *   &#64;Override
+ *   public ExternalOrderType orderTypeToExternalOrderType(OrderType orderType) {
+ *      if ( orderType == null ) {
+ *          return null;
+ *      }
+ *
+ *      ExternalOrderType externalOrderType_;
+ *
+ *      switch ( orderType ) {
+ *          case EXTRA: externalOrderType_ = ExternalOrderType.SPECIAL;
+ *           break;
+ *           case STANDARD: externalOrderType_ = ExternalOrderType.DEFAULT;
+ *           break;
+ *           case NORMAL: externalOrderType_ = ExternalOrderType.DEFAULT;
+ *           break;
+ *           case RETAIL: externalOrderType_ = ExternalOrderType.RETAIL;
+ *           break;
+ *           case B2B: externalOrderType_ = ExternalOrderType.B2B;
+ *           break;
+ *           default: throw new IllegalArgumentException( "Unexpected enum constant: " + orderType );
+ *      }
+ *
+ *      return externalOrderType_;
+ *   }
+ * </code>
+ * </pre>
+ *
+ * You can specify how to handle null sources by specifying a mapping. And, you can specify how to handle defaults.
+ *
+ * <pre>
+ * <code>
+ * &#64;ValueMappings({
+ *     &#64;ValueMapping( valueMappingType = ValueMappingType.NULL, target = "DEFAULT" ),
+ *     &#64;ValueMapping( source = "STANDARD", targetIsNull = true ),
+ *     &#64;ValueMapping( valueMappingType = ValueMappingType.DEFAULT, target = "SPECIAL" ) })
+ * ExternalOrderType orderTypeToExternalOrderType(OrderType orderType);
+ * </code>
+ * </pre>
+ *
+ * That will result in:
+ *
+ * <pre>
+ * <code>
+ * &#64;Override
+ * public ExternalOrderType orderTypeToExternalOrderType(OrderType orderType) {
+ *      if ( orderType == null ) {
+ *         return ExternalOrderType.DEFAULT;
+ *      }
+ *
+ *      ExternalOrderType externalOrderType_;
+ *
+ *      switch ( orderType ) {
+ *          case STANDARD: externalOrderType_ = null;
+ *          break;
+ *          default: externalOrderType_ = ExternalOrderType.SPECIAL;
+ *      }
+ *
+ *      return externalOrderType_;
+ *
+ *  }
+ * </code>
+ * </pre>
+ *
+ * What about only specifying the default mapping, and let the remainder be done by name based mapping.
+ *
+ * <pre>
+ * <code>
+ *  &#64;ValueMapping( valueMappingType = ValueMappingType.DEFAULT_AFTER_APPLYING_NAME_BASED_MAPPINGS,
+ *  target = "DEFAULT" )
+ *  ExternalOrderType orderTypeToExternalOrderType(OrderType orderType);
+ * </code>
+ * </pre>
+ *
+ * Will result in:
+ *
+ * <pre>
+ * <code>
+ * &#64;Override
+ * public ExternalOrderType orderTypeToExternalOrderType(OrderType orderType) {
+ *       if ( orderType == null ) {
+ *          return null;
+ *       }
+ *
+ *       ExternalOrderType externalOrderType_;
+ *
+ *       switch ( orderType ) {
+ *          case RETAIL: externalOrderType_ = ExternalOrderType.RETAIL;
+ *          break;
+ *          case B2B: externalOrderType_ = ExternalOrderType.B2B;
+ *          break;
+ *          default: externalOrderType_ = ExternalOrderType.DEFAULT;
+ *        }
+ *
+ *        return externalOrderType_;
+ *    }
+ * </code>
+ * </pre>
+ *
+ * So: MapStruct handles the same names and reverts to default for the ones it cannot map based on name.
+ *
  * @author Sjaak Derksen
  */
 @Repeatable(ValueMappings.class)
