5d0bc08e2e32e504993fc5131a6b427fc2875cba
Markus Heberling <markus.heberling@hpe.com>
2016-08-18 11:29:01 +0200

    #855 Show incorrect ordering of properties

---
+++ processor/src/test/java/org/mapstruct/ap/test/dependency/DemoDTO.java
@@ -0,0 +1,52 @@
+/**
+ *  Copyright 2012-2016 Gunnar Morling (http://www.gunnarmorling.de/)
+ *  and/or other contributors as indicated by the @authors tag. See the
+ *  copyright.txt file in the distribution for a full listing of all
+ *  contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.mapstruct.ap.test.dependency;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class DemoDTO {
+    private List<String> order = new LinkedList<String>();
+
+    public void setField0(String field0) {
+        order.add( "field0" );
+    }
+
+    public void setField1(String field1) {
+        order.add( "field1" );
+    }
+
+    public void setField2(String field2) {
+        order.add( "field2" );
+    }
+
+
+    public void setField3(String field3) {
+        order.add( "field3" );
+    }
+
+
+    public void setField4(String field4) {
+        order.add( "field4" );
+    }
+
+    public List<String> getOrder() {
+        return order;
+    }
+}
