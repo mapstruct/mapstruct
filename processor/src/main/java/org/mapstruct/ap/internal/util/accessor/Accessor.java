a4e9c211d3dd83f7d166f182e693d4d417298759
Filip Hrisafov <filip.hrisafov@gmail.com>
2016-12-17 15:16:53 +0100

    #557 add Javadoc for the Accessor

--- processor/src/main/java/org/mapstruct/ap/internal/util/accessor/Accessor.java
+++ processor/src/main/java/org/mapstruct/ap/internal/util/accessor/Accessor.java
@@ -26,17 +26,39 @@ import javax.lang.model.element.Name;
 import javax.lang.model.type.TypeMirror;
 
 /**
+ * This represents an Accessor that can be used for writing/reading a property to/from a bean.
+ *
  * @author Filip Hrisafov
  */
 public interface Accessor {
 
+    /**
+     * This returns the type that this accessor gives as a return.
+     *
+     * e.g. The {@link ExecutableElement#getReturnType()} if this is a method accessor,
+     * or {@link javax.lang.model.element.VariableElement#asType()} for field accessors.
+     *
+     * @return the type that the accessor gives as a return
+     */
     TypeMirror getAccessedType();
 
+    /**
+     * @return the simple name of the accessor
+     */
     Name getSimpleName();
 
+    /**
+     * @return the set of modifiers that the accessor has
+     */
     Set<Modifier> getModifiers();
 
+    /**
+     * @return the {@link ExecutableElement}, or {@code null} if the accessor does not have one
+     */
     ExecutableElement getExecutable();
 
+    /**
+     * @return the underlying {@link Element}
+     */
     Element getElement();
 }
