5e52755008b1e24f5cd3ed30c4e43b2c89227fe7
Filip Hrisafov <filip.hrisafov@gmail.com>
2017-03-23 19:51:21 +0100

    #1155 Field accessors should be considered when resolving nested target properties

---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_1155/Issue1155Test.java
@@ -0,0 +1,50 @@
+/**
+ *  Copyright 2012-2017 Gunnar Morling (http://www.gunnarmorling.de/)
+ *  and/or other contributors as indicated by the @authors tag. See the
+ *  copyright.txt file in the distribution for a full listing of all
+ *  contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.mapstruct.ap.test.bugs._1155;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mapstruct.ap.testutil.IssueKey;
+import org.mapstruct.ap.testutil.WithClasses;
+import org.mapstruct.ap.testutil.runner.AnnotationProcessorTestRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Filip Hrisafov
+ */
+@WithClasses({
+    Entity.class,
+    Issue1155Mapper.class
+})
+@RunWith(AnnotationProcessorTestRunner.class)
+@IssueKey("1155")
+public class Issue1155Test {
+
+    @Test
+    public void shouldCompile() throws Exception {
+
+        Entity.Dto dto = new Entity.Dto();
+        dto.clientId = 10;
+        Entity entity = Issue1155Mapper.INSTANCE.toEntity( dto );
+
+        assertThat( entity.client ).isNotNull();
+        assertThat( entity.client.id ).isEqualTo( 10 );
+    }
+}
