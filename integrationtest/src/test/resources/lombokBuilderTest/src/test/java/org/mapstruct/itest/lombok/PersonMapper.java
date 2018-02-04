70419f91b0725fee1b2d5e009eb29e13b2a4824d
Filip Hrisafov <filip.hrisafov@gmail.com>
2018-02-04 12:18:30 +0100

    #782 The builder type from lombok is not present during the annotation processing phase

--- integrationtest/src/test/resources/lombokBuilderTest/src/test/java/org/mapstruct/itest/lombok/PersonMapper.java
+++ integrationtest/src/test/resources/lombokBuilderTest/src/test/java/org/mapstruct/itest/lombok/PersonMapper.java
@@ -22,6 +22,12 @@ import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
+/**
+ * The Builder creation method is not present during the annotation processing of the mapper.
+ * Therefore we put the mapper into a separate class.
+ *
+ * @see <span>Lombok Issue <a href="https://github.com/rzwitserloot/lombok/issues/1538">#1538</a></span>
+ */
 @Mapper(unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface PersonMapper {
 
