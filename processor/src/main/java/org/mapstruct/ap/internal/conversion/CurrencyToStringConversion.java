2225bacb57ff330f979f87f97ad61fb96bf8ca66
Darren Rambaud <darren@rambaud.io>
2018-02-16 23:46:34 -0600

    #1355: Setting up the test(s) for new conversion between java.util.Currency and String

---
+++ processor/src/main/java/org/mapstruct/ap/internal/conversion/CurrencyToStringConversion.java
@@ -0,0 +1,9 @@
+package org.mapstruct.ap.internal.conversion;
+
+/**
+ * @author Darren Rambaud (2/16/18)
+ * <p>
+ * darren@rambaud.io
+ */
+public class CurrencyToStringConversion {
+}
---
+++ processor/src/test/java/org/mapstruct/ap/test/conversion/currency/CurrencyMapper.java
@@ -0,0 +1,9 @@
+package org.mapstruct.ap.test.conversion.currency;
+
+/**
+ * @author Darren Rambaud (2/16/18)
+ * <p>
+ * darren@rambaud.io
+ */
+public class CurrencyMapper {
+}
