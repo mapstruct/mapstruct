8f88c6baa70b33e2a2085ccfd922e15900a00425
Darren Rambaud <xyzst@users.noreply.github.com>
2018-02-18 12:02:56 -0600

    #1355: Adds Implicit Conversion Between java.util.Currency <~> String (#1381)
    * #1355: Setting up the test(s) for new conversion between java.util.Currency and String
    
    * #1355: Added SimpleConversion subclass to convert a Currency object to a String object and vice-versa, and registered the class to Conversions
    
    * #1355: Initial tests written, may need to re-write some test files for readability and/or add more test case(s). Basic tests are passing at this time
    
    * #1355: Added copyright statement, added documentation for new implicit conversion
    
    * #1355: Added clarity to documentation
    
    * #1355: Replaced use of one letter variables
    
    * #1355: Resolved CheckStyle errors
    
    * #1355: Fixes license header spacing so the license plugin no longer fails the build
    
    * Small cleanups

---
+++ processor/src/test/java/org/mapstruct/ap/test/conversion/currency/CurrencyTarget.java
@@ -0,0 +1,46 @@
+/**
+ *  Copyright 2012-2017 Gunnar Morling (http://www.gunnarmorling.de/)
+ *  and/or other contributors as indicated by the @authors tag. See the
+ *  copyright.txt file in the distribution for a full listing of all
+ *  contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.mapstruct.ap.test.conversion.currency;
+
+import java.util.Set;
+
+/**
+ * @author Darren Rambaud
+ */
+public class CurrencyTarget {
+
+    private String currencyA;
+    private Set<String> uniqueCurrencies;
+
+    public String getCurrencyA() {
+        return this.currencyA;
+    }
+
+    public void setCurrencyA(final String currencyA) {
+        this.currencyA = currencyA;
+    }
+
+    public Set<String> getUniqueCurrencies() {
+        return this.uniqueCurrencies;
+    }
+
+    public void setUniqueCurrencies(final Set<String> uniqueCurrencies) {
+        this.uniqueCurrencies = uniqueCurrencies;
+    }
+}
