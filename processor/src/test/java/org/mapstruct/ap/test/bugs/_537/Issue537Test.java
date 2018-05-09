eeddc17de4178ef7aedbf07f7862aed79790033b
Christian Bandowski <christian@myvm.de>
2018-05-09 22:28:17 +0200

    #537 add unit test

---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_537/Issue537Test.java
@@ -0,0 +1,50 @@
+/**
+ *  Copyright 2012-2017 Gunnar Morling (http://www.gunnarmorling.de/)
+ *  and/or other contributors as indicated by the @authors tag. See the
+ *  copyright.txt file in the distribution for a full listing of all
+ *  contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.mapstruct.ap.test.bugs._537;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mapstruct.ap.testutil.IssueKey;
+import org.mapstruct.ap.testutil.WithClasses;
+import org.mapstruct.ap.testutil.runner.AnnotationProcessorTestRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Christian Bandowski
+ */
+@RunWith(AnnotationProcessorTestRunner.class)
+@IssueKey("537")
+@WithClasses({
+    Issue537Mapper.class,
+    Issue537MapperConfig.class,
+    ReferenceMapper.class,
+    Source.class,
+    Target.class
+})
+public class Issue537Test {
+
+    @Test
+    public void testThatReferencedMapperWillBeUsed() {
+        Target target = Issue537Mapper.INSTANCE.mapDto( new Source( "abc" ) );
+
+        assertThat( target ).isNotNull();
+        assertThat( target.getValue() ).isEqualTo( 3 );
+    }
+}
