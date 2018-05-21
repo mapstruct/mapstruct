f8a0ba69862c180f3cea68d3d8b33de86b306cc0
Cindy Wang <xwang@appnexus.com>
2018-05-21 16:02:58 -0400

    issue #1456,apply source presence tracking recursively

---
+++ processor/src/test/java/org/mapstruct/ap/test/source/presencecheck/spi/PresenceCheckNestedObjectsTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.source.presencecheck.spi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mapstruct.ap.testutil.WithClasses;
+import org.mapstruct.ap.testutil.runner.AnnotationProcessorTestRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Test for correct handling of source presence checks for nested objects.
+ *
+ * @author Cindy Wang
+ */
+@WithClasses({
+    SoccerTeamMapperNestedObjects.class,
+    SoccerTeamSource.class,
+    GoalKeeper.class,
+    SoccerTeamTargetWithPresenceCheck.class
+})
+@RunWith(AnnotationProcessorTestRunner.class)
+public class PresenceCheckNestedObjectsTest {
+
+    @Test
+    public void testNestedWithSourcesAbsentOnNestingLevel() {
+
+        SoccerTeamSource soccerTeamSource = new SoccerTeamSource();
+        GoalKeeper goalKeeper = new GoalKeeper();
+        goalKeeper.setHasName( false );
+        soccerTeamSource.setGoalKeeper( goalKeeper );
+
+        SoccerTeamTargetWithPresenceCheck target = SoccerTeamMapperNestedObjects.INSTANCE.mapNested( soccerTeamSource );
+
+        assertThat( target.getGoalKeeperName() ).isNull();
+        assertFalse( target.hasGoalKeeperName() );
+    }
+}
---
+++ processor/src/test/java/org/mapstruct/ap/test/source/presencecheck/spi/SoccerTeamMapperNestedObjects.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.source.presencecheck.spi;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * @author Cindy Wang
+ */
+@Mapper
+public interface SoccerTeamMapperNestedObjects {
+    SoccerTeamMapperNestedObjects INSTANCE = Mappers.getMapper( SoccerTeamMapperNestedObjects.class );
+
+    @Mappings({
+        @Mapping(target = "players", ignore = true),
+        @Mapping(target = "goalKeeperName", source = "goalKeeper.name")
+    })
+    SoccerTeamTargetWithPresenceCheck mapNested( SoccerTeamSource in );
+
+}
