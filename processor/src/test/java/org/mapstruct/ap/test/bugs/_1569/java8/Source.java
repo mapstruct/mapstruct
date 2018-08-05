c38e0c02c1ab3a02b8934d457e6f97ed8f3fec7c
Filip Hrisafov <filip.hrisafov@gmail.com>
2018-08-05 21:14:19 +0200

    #1569 Make sure that converting Java 8 LocalDate to Date generates correct code
    * ZoneOffset instead of ZoneId needs to be imported when performing a conversion from LocalDate to Date

---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_1569/java8/Source.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.bugs._1569.java8;
+
+import java.util.Date;
+
+/**
+ * @author Filip Hrisafov
+ */
+public class Source {
+
+    private Date promotionDate;
+
+    public Date getPromotionDate() {
+        return promotionDate;
+    }
+
+    public void setPromotionDate(Date promotionDate) {
+        this.promotionDate = promotionDate;
+    }
+}
---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_1569/java8/Target.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.bugs._1569.java8;
+
+import java.time.LocalDate;
+
+/**
+ * @author Filip Hrisafov
+ */
+public class Target {
+
+    private LocalDate promotionDate;
+
+    public LocalDate getPromotionDate() {
+        return promotionDate;
+    }
+
+    public void setPromotionDate(LocalDate promotionDate) {
+        this.promotionDate = promotionDate;
+    }
+}
