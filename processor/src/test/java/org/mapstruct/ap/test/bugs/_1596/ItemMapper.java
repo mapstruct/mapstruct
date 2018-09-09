a3dcd662a478e3dd9d9447071a50ae543cb74144
sjaakd <sjaakderksen@zonnet.nl>
2018-09-09 12:43:06 +0200

    #1569 Reproducer missing Immubable buildertype import

---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_1596/ItemMapper.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.bugs._1596;
+
+import org.mapstruct.Builder;
+import org.mapstruct.Mapper;
+import org.mapstruct.ap.test.bugs._1596.domain.Item;
+import org.mapstruct.ap.test.bugs._1596.dto.ItemDTO;
+import org.mapstruct.factory.Mappers;
+
+@Mapper( builder = @Builder)
+public abstract class ItemMapper {
+
+    public static final ItemMapper INSTANCE = Mappers.getMapper( ItemMapper.class );
+
+    public abstract Item map(ItemDTO itemDTO);
+}
