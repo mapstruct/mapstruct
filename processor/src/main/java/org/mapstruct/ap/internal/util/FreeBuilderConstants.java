5ebde694f19a7d07e5c3f5504749aa978efa9662
Filip Hrisafov <filip.hrisafov@gmail.com>
2018-10-28 17:33:28 +0100

    Add a FreeBuilder specific AccessNamingStrategy
    The builder created by FreeBuilder have different fluent methods created.
    When using FreeBuilder one needs to use the JavaBean convention for the getters (as otherwise MapStruct won't recognize them),
    this leads to FreeBuilder creating fluent setters that start with set (so we can safely ignore all other fluent setters)

---
+++ processor/src/main/java/org/mapstruct/ap/internal/util/FreeBuilderConstants.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.internal.util;
+
+/**
+ * Helper for holding FreeBuilder FQN.
+ *
+ * @author Filip Hrisafov
+ */
+public class FreeBuilderConstants {
+
+    public static final String FREE_BUILDER_FQN = "org.inferred.freebuilder.FreeBuilder";
+
+    private FreeBuilderConstants() {
+
+    }
+}
---
+++ processor/src/main/java/org/mapstruct/ap/spi/FreeBuilderAccessorNamingStrategy.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.spi;
+
+import javax.lang.model.element.ExecutableElement;
+
+/**
+ * Accessor naming strategy for FreeBuilder.
+ * FreeBuilder adds a lot of other methods that can be considered as fluent setters. Such as:
+ * <ul>
+ *     <li>{@code from(Target)}</li>
+ *     <li>{@code mapXXX(UnaryOperator)}</li>
+ *     <li>{@code mutateXXX(Consumer)}</li>
+ *     <li>{@code mergeFrom(Target)}</li>
+ *     <li>{@code mergeFrom(Target.Builder)}</li>
+ * </ul>
+ * <p>
+ * When the JavaBean convention is not used with FreeBuilder then the getters are non standard and MapStruct
+ * won't recognize them. Therefore one needs to use the JavaBean convention in which the fluent setters
+ * start with {@code set}.
+ *
+ * @author Filip Hrisafov
+ */
+public class FreeBuilderAccessorNamingStrategy extends DefaultAccessorNamingStrategy {
+
+    @Override
+    protected boolean isFluentSetter(ExecutableElement method) {
+        // When using FreeBuilder one needs to use the JavaBean convention, which means that all setters will start
+        // with set
+        return false;
+    }
+}
