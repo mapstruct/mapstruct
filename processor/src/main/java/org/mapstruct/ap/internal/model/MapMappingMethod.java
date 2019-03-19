66055f024b06b979b93a76b13a78ec70b5098d45
Sjaak Derksen <sjaakd@users.noreply.github.com>
2019-03-19 09:44:27 +0100

    #1756 better forged method error based on empty target bean properties (#1757)

--- processor/src/main/java/org/mapstruct/ap/internal/model/MapMappingMethod.java
+++ processor/src/main/java/org/mapstruct/ap/internal/model/MapMappingMethod.java
@@ -184,7 +184,6 @@ public class MapMappingMethod extends NormalTypeMappingMethod {
                     .getFactoryMethod( method, null, ctx );
             }
 
-
             keyAssignment = new LocalVarWrapper( keyAssignment, method.getThrownTypes(), keyTargetType, false );
             valueAssignment = new LocalVarWrapper( valueAssignment, method.getThrownTypes(), valueTargetType, false );
 
