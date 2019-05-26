2f36a41735f05e3c861a2cfd84ade057ad6e074f
Jonathan Kraska <jakraska@gmail.com>
2019-05-26 11:41:44 -0400

    #1826: fixed null pointer in nested property mapping when using presence checking (#1827)

---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_1826/Target.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.bugs._1826;
+
+public class Target {
+
+  private String content;
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+}
