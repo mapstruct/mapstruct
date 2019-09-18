d018aed25150d886b54d41099345908c01d4cc7b
Filip Hrisafov <filip.hrisafov@gmail.com>
2019-09-18 13:09:40 +0200

    #1790 Use mapperPrism.values.nullValuePropertyMappingStrategy when retrieving NullValuePropertyMappingStrategy

---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_1790/Target.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.bugs._1790;
+
+/**
+ * @author Filip Hrisafov
+ */
+public class Target {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
