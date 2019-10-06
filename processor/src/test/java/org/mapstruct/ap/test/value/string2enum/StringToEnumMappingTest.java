5fac130de8a330b7832268b4a231bc62b88a6ad8
sjaakd <sjaakderksen@zonnet.nl>
2019-10-06 14:58:05 +0200

    Headers..

--- processor/src/test/java/org/mapstruct/ap/test/value/string2enum/StringToEnumMappingTest.java
+++ processor/src/test/java/org/mapstruct/ap/test/value/string2enum/StringToEnumMappingTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package org.mapstruct.ap.test.value.string2enum;
 
 import org.junit.Test;
