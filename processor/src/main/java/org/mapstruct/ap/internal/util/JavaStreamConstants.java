c28a61f02286d5d50d67ca2d03f58d2c9a44f24a
Andrei Arlou <andrei.arlou@gmail.com>
2019-10-19 00:50:46 +0300

    #1944 Remove unused constants in JavaStreamConstants

--- processor/src/main/java/org/mapstruct/ap/internal/util/JavaStreamConstants.java
+++ processor/src/main/java/org/mapstruct/ap/internal/util/JavaStreamConstants.java
@@ -13,10 +13,6 @@ package org.mapstruct.ap.internal.util;
 public final class JavaStreamConstants {
 
     public static final String STREAM_FQN = "java.util.stream.Stream";
-    public static final String COLLECTORS_FQN = "java.util.stream.Collectors";
-    public static final String STREAM_SUPPORT_FQN = "java.util.stream.StreamSupport";
-    public static final String OPTIONAL_FQN = "java.util.Optional";
-    public static final String FUNCTION_FQN = "java.util.function.Function";
 
     private JavaStreamConstants() {
     }
