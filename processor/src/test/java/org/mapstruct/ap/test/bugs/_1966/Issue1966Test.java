20c927f6d57f77c57e3be252ced54f038cd67dc5
sjaakd <sjaakderksen@zonnet.nl>
2019-11-10 19:41:32 +0100

    #1966 extra unit test

---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_1966/Issue1966Test.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.bugs._1966;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mapstruct.ap.testutil.IssueKey;
+import org.mapstruct.ap.testutil.WithClasses;
+import org.mapstruct.ap.testutil.runner.AnnotationProcessorTestRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Sjaak Derksen
+ */
+@IssueKey("1966")
+@RunWith(AnnotationProcessorTestRunner.class)
+@WithClasses({
+    Issue1966Mapper.class
+})
+public class Issue1966Test {
+
+    @Test
+    public void shouldSelectDefaultExpression() {
+
+        Issue1966Mapper.AnimalRecord dto = new Issue1966Mapper.AnimalRecord();
+
+        Issue1966Mapper.Animal entity = Issue1966Mapper.INSTANCE.toAnimal( dto );
+
+        assertThat( entity.getPreviousNames() ).isNotNull();
+        assertThat( entity.getPreviousNames() ).isEmpty();
+    }
+}
