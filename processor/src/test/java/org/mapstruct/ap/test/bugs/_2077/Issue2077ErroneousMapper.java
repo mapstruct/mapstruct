204f35e3060fef3b5739f3857b8575e8e605fcac
sjaakd <sjaakderksen@zonnet.nl>
2020-04-25 12:04:45 +0200

    #2077 nullpointer due to no-getter source

---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_2077/Issue2077ErroneousMapper.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.bugs._2077;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * @author Sjaak Derksen
+ */
+@Mapper( unmappedTargetPolicy = ReportingPolicy.ERROR )
+public interface Issue2077ErroneousMapper {
+
+    Issue2077ErroneousMapper INSTANCE = Mappers.getMapper( Issue2077ErroneousMapper.class );
+
+    @Mapping(target = "s1")
+    Target map(String source);
+
+    class Target {
+
+        private String s1;
+
+        public String getS1() {
+            return s1;
+        }
+
+        public void setS1(String s1) {
+            this.s1 = s1;
+        }
+
+    }
+}
---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_2077/Issue2077Test.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.bugs._2077;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mapstruct.ap.testutil.IssueKey;
+import org.mapstruct.ap.testutil.WithClasses;
+import org.mapstruct.ap.testutil.compilation.annotation.CompilationResult;
+import org.mapstruct.ap.testutil.compilation.annotation.Diagnostic;
+import org.mapstruct.ap.testutil.compilation.annotation.ExpectedCompilationOutcome;
+import org.mapstruct.ap.testutil.runner.AnnotationProcessorTestRunner;
+
+import static javax.tools.Diagnostic.Kind.ERROR;
+
+/**
+ * @author Sjaak Derksen
+ */
+@IssueKey("2077")
+@RunWith(AnnotationProcessorTestRunner.class)
+public class Issue2077Test {
+
+    @Test
+    @WithClasses(Issue2077ErroneousMapper.class)
+    @ExpectedCompilationOutcome(
+        value = CompilationResult.FAILED,
+        diagnostics = {
+            @Diagnostic(type = Issue2077ErroneousMapper.class,
+                kind = ERROR,
+                line = 22,
+                message = "Unmapped target property: \"s1\".")
+        }
+    )
+    public void shouldNotCompile() {
+    }
+}
