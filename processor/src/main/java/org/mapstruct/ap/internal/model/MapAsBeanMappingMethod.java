6ab92230b06b3f2276d71c0d1e79f1d905935f66
sjaakd <sjaakderksen@zonnet.nl>
2020-06-07 09:12:05 +0200

    wip

--- processor/src/main/java/org/mapstruct/ap/internal/model/MapAsBeanMappingMethod.java
+++ processor/src/main/java/org/mapstruct/ap/internal/model/MapAsBeanMappingMethod.java
@@ -1,4 +1,12 @@
 package org.mapstruct.ap.internal.model;
 
+import java.util.List;
+
+import org.mapstruct.ap.internal.model.common.Type;
+
 public class MapAsBeanMappingMethod {
+
+    public MapAsBeanMappingMethod( List<Type> targetType) {
+
+    }
 }
