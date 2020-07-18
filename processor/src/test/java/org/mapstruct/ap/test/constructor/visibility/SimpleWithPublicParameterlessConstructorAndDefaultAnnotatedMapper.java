ff150cb2ee9de4f7d71d63ca82eb069a6accc306
Filip Hrisafov <filip.hrisafov@gmail.com>
2020-07-18 10:54:09 +0200

    #2150 Change the rules for how a constructor for mapping is picked
    New rules:
    
    1. Constructor annotated with @Default (from any package) has highest precedence
    2. If there is a single public constructor then it would be used to construct the object
    3. If a parameterless constructor exists then it would be used to construct the object, and the other constructors will be ignored

---
+++ processor/src/test/java/org/mapstruct/ap/test/constructor/visibility/SimpleWithPublicParameterlessConstructorAndDefaultAnnotatedMapper.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.constructor.visibility;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ap.test.constructor.Default;
+import org.mapstruct.ap.test.constructor.PersonDto;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * @author Filip Hrisafov
+ */
+@Mapper
+public interface SimpleWithPublicParameterlessConstructorAndDefaultAnnotatedMapper {
+
+    SimpleWithPublicParameterlessConstructorAndDefaultAnnotatedMapper INSTANCE = Mappers.getMapper(
+        SimpleWithPublicParameterlessConstructorAndDefaultAnnotatedMapper.class );
+
+    Person map(PersonDto dto);
+
+    class Person {
+
+        private final String name;
+        private final int age;
+
+        protected Person() {
+            this( "From Constructor", -1 );
+        }
+
+        protected Person(String name) {
+            this( name, -1 );
+        }
+
+        @Default
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
+}
---
+++ processor/src/test/java/org/mapstruct/ap/test/constructor/visibility/SimpleWithPublicParameterlessConstructorMapper.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.constructor.visibility;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ap.test.constructor.PersonDto;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * @author Filip Hrisafov
+ */
+@Mapper
+public interface SimpleWithPublicParameterlessConstructorMapper {
+
+    SimpleWithPublicParameterlessConstructorMapper INSTANCE = Mappers.getMapper(
+        SimpleWithPublicParameterlessConstructorMapper.class );
+
+    Person map(PersonDto dto);
+
+    class Person {
+
+        private final String name;
+        private final int age;
+
+        public Person() {
+            this( "From Constructor", -1 );
+        }
+
+        protected Person(String name) {
+            this( name, -1 );
+        }
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
+}
