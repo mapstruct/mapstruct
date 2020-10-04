3678be71cbccf537be63f5f266d5c05d8d06275e
Mark Ford <mark@massfords.com>
2020-10-04 11:39:09 -0400

    #2198 - added missing license header

--- integrationtest/src/test/resources/immutablesBuilderTest/mapper/src/test/java/org/mapstruct/itest/immutables/style/StyledValueEnclosingTest.java
+++ integrationtest/src/test/resources/immutablesBuilderTest/mapper/src/test/java/org/mapstruct/itest/immutables/style/StyledValueEnclosingTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package org.mapstruct.itest.immutables.style;
 
 import org.junit.Test;
