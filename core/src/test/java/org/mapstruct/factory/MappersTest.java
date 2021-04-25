627be5308855aa257826beeaccf570390de195f0
Filip Hrisafov <filip.hrisafov@gmail.com>
2021-04-25 12:39:04 +0200

    Migrate mapstruct core tests to JUnit Jupiter

--- core/src/test/java/org/mapstruct/factory/MappersTest.java
+++ core/src/test/java/org/mapstruct/factory/MappersTest.java
@@ -7,7 +7,7 @@ package org.mapstruct.factory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.mapstruct.test.model.Foo;
 import org.mapstruct.test.model.SomeClass;
 
