29119fd33426ccf9d3ec931ac813ceaf6ec9f113
Filip Hrisafov <filip.hrisafov@gmail.com>
2021-06-26 11:28:37 +0200

    #2501 Add test case

---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_2501/Issue2501Test.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.bugs._2501;
+
+import org.mapstruct.ap.testutil.ProcessorTest;
+import org.mapstruct.ap.testutil.WithClasses;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Filip Hrisafov
+ */
+@WithClasses({
+    Issue2501Mapper.class
+})
+class Issue2501Test {
+
+    @ProcessorTest
+    void shouldUnwrapEnumOptional() {
+        Issue2501Mapper.CustomerDTO source = new Issue2501Mapper.CustomerDTO();
+        source.setStatus( Issue2501Mapper.DtoStatus.DISABLED );
+
+        Issue2501Mapper.Customer target = Issue2501Mapper.INSTANCE.map( source );
+
+        assertThat( target.getStatus() ).isEqualTo( Issue2501Mapper.CustomerStatus.DISABLED );
+    }
+}
