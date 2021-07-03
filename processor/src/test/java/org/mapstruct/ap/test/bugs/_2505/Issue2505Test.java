1ed4fb2d4cb30c58fc5de2471a9bfcb9be592b61
sjaakd <sjaakderksen@zonnet.nl>
2021-07-03 12:02:01 +0200

    #2505 deepclone generates enum mapping method reproducer

---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_2505/Issue2505Test.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.bugs._2505;
+
+import org.mapstruct.ap.testutil.ProcessorTest;
+import org.mapstruct.ap.testutil.WithClasses;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Sjaak derksen
+ */
+@WithClasses({
+    Issue2505Mapper.class
+})
+class Issue2505Test {
+
+    @ProcessorTest
+    void shouldNotGenerateEnumMappingMethodForDeepClone() {
+        Issue2505Mapper.CustomerDTO source = new Issue2505Mapper.CustomerDTO();
+        source.setStatus( Issue2505Mapper.Status.DISABLED );
+
+        Issue2505Mapper.Customer target = Issue2505Mapper.INSTANCE.map( source );
+
+        assertThat( target.getStatus() ).isEqualTo( Issue2505Mapper.Status.DISABLED );
+    }
+}
