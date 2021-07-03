a95d1c59c3f531fc90b62c60092dd9c16bc93b66
Sjaak Derksen <sjaakd@users.noreply.github.com>
2021-07-03 15:28:10 +0200

    #2505 deepclone generates enum mapping method (#2507)

---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_2505/Issue2505Mapper.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.bugs._2505;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.control.DeepClone;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * @author Sjaak Derksen
+ */
+@Mapper( mappingControl = DeepClone.class )
+public interface Issue2505Mapper {
+
+    Issue2505Mapper INSTANCE = Mappers.getMapper( Issue2505Mapper.class );
+
+    Customer map(CustomerDTO value);
+
+    enum Status {
+        ENABLED, DISABLED,
+    }
+
+    class Customer {
+
+        private Status status;
+
+        public Status getStatus() {
+            return status;
+        }
+
+        public void setStatus(Status stat) {
+            this.status = stat;
+        }
+    }
+
+    class CustomerDTO {
+
+        private Status status;
+
+        public Status getStatus() {
+            return status;
+        }
+
+        public void setStatus(Status status) {
+            this.status = status;
+        }
+    }
+}
