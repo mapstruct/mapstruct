5da4476ca07a8ee23a0c3a5630ed217b8e824d11
Ben Zegveld <Ben.Zegveld@gmail.com>
2021-11-02 13:58:57 +0100

    #2636: defaultValue combined with qualified should not convert if not needed.

---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_2636/Issue2636Mapper.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.bugs._2636;
+
+import java.util.UUID;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+/**
+ * @author Ben Zegveld
+ */
+@Mapper
+public interface Issue2636Mapper {
+    @Mapping( source = "folder.ancestor.id", target = "parent", defaultValue = "#", qualifiedByName = "uuidToString" )
+    DirectoryNode convert(Folder folder);
+
+    @Named( "uuidToString" )
+    default String uuidToString(UUID id) {
+        return id.toString();
+    }
+}
+
+class DirectoryNode {
+    private String parent;
+
+    public void setParent(String parent) {
+        this.parent = parent;
+    }
+
+    public String getParent() {
+        return parent;
+    }
+}
+
+class Folder {
+    private String id;
+    private Folder ancestor;
+
+    Folder(String id, Folder ancestor) {
+        this.id = id;
+        this.ancestor = ancestor;
+    }
+
+    public Folder getAncestor() {
+        return ancestor;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
