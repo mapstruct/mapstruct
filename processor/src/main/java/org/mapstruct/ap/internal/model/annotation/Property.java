b9299b3a519380d75e3d1efe2277f8abfefe42f4
Ben Zegveld <Ben.Zegveld@gmail.com>
2022-03-18 20:22:27 +0100

    #1574: add missing license header.

--- processor/src/main/java/org/mapstruct/ap/internal/model/annotation/Property.java
+++ processor/src/main/java/org/mapstruct/ap/internal/model/annotation/Property.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package org.mapstruct.ap.internal.model.annotation;
 
 import java.util.Collections;
@@ -6,6 +11,9 @@ import java.util.Set;
 import org.mapstruct.ap.internal.model.common.ModelElement;
 import org.mapstruct.ap.internal.model.common.Type;
 
+/**
+ * @author Ben Zegveld
+ */
 public abstract class Property extends ModelElement {
 
     private final String key;
