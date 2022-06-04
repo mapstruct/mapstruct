86268b8e2715c22c4b2eb1545b06ac6d1e2cdedb
Ben Zegveld <Ben.Zegveld@gmail.com>
2022-06-04 09:08:07 +0200

    #1574: missing javadoc and fixed long lines.

--- processor/src/test/java/org/mapstruct/ap/test/annotatewith/ErroneousMapperWithWrongParameter.java
+++ processor/src/test/java/org/mapstruct/ap/test/annotatewith/ErroneousMapperWithWrongParameter.java
@@ -13,23 +13,22 @@ import org.mapstruct.Mapper;
  */
 @Mapper
 @AnnotateWith( value = CustomAnnotationWithParams.class,
-           elements = {
-                    @AnnotateWith.Element( name = "stringParam", booleans = true ),
-                    @AnnotateWith.Element( name = "stringParam", bytes = 0x12 ),
-                    @AnnotateWith.Element( name = "stringParam", chars = 'a' ),
-                    @AnnotateWith.Element( name = "stringParam", classes = CustomAnnotationWithParams.class ),
-                    @AnnotateWith.Element( name = "stringParam", doubles = 12.34 ),
-                    @AnnotateWith.Element( name = "stringParam", floats = 12.34f ),
-                    @AnnotateWith.Element( name = "stringParam", ints = 1234 ),
-                    @AnnotateWith.Element( name = "stringParam", longs = 1234L ),
-                    @AnnotateWith.Element( name = "stringParam", shorts = 12 ),
-                    @AnnotateWith.Element( name = "stringParam", strings = "correctValue" ),
-                    @AnnotateWith.Element( name = "genericTypedClass", strings = "wrong" ),
-                    @AnnotateWith.Element( name = "genericTypedClass", classes = ErroneousMapperWithWrongParameter.class ),
-                    @AnnotateWith.Element( name = "enumParam", enums = @AnnotateWith.EnumElement(
-                        enumClass = WrongAnnotateWithEnum.class, name = "EXISTING" ) )
-           }
-)
+       elements = {
+            @AnnotateWith.Element( name = "stringParam", booleans = true ),
+            @AnnotateWith.Element( name = "stringParam", bytes = 0x12 ),
+            @AnnotateWith.Element( name = "stringParam", chars = 'a' ),
+            @AnnotateWith.Element( name = "stringParam", classes = CustomAnnotationWithParams.class ),
+            @AnnotateWith.Element( name = "stringParam", doubles = 12.34 ),
+            @AnnotateWith.Element( name = "stringParam", floats = 12.34f ),
+            @AnnotateWith.Element( name = "stringParam", ints = 1234 ),
+            @AnnotateWith.Element( name = "stringParam", longs = 1234L ),
+            @AnnotateWith.Element( name = "stringParam", shorts = 12 ),
+            @AnnotateWith.Element( name = "stringParam", strings = "correctValue" ),
+            @AnnotateWith.Element( name = "genericTypedClass", strings = "wrong" ),
+            @AnnotateWith.Element( name = "genericTypedClass", classes = ErroneousMapperWithWrongParameter.class ),
+            @AnnotateWith.Element( name = "enumParam", enums = @AnnotateWith.EnumElement(
+                enumClass = WrongAnnotateWithEnum.class, name = "EXISTING" ) )
+       } )
 public interface ErroneousMapperWithWrongParameter {
 
 }
