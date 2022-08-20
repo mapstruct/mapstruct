e5c7fdb2f68492b13e63cc3ea5e38cb6166b6f3d
Hakan <o.hakanozkan@gmail.com>
2022-08-20 15:23:32 +0200

    #2839 Keep thrown types when creating a new ForgedMethod with the same arguments
    This fixes a compilation error when mapping fields with the same type due to not wrapping in a `try-catch` block

---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_2839/Issue2839Exception.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.bugs._2839;
+
+/**
+ * @author Hakan Özkan
+ */
+public class Issue2839Exception extends Exception {
+
+    public Issue2839Exception(String message) {
+        super( message );
+    }
+}
