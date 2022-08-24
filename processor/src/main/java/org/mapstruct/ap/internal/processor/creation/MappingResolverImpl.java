b63c2bd693dc6878a95bad7ebda9271b61c5ab51
Filip Hrisafov <filip.hrisafov@gmail.com>
2022-08-24 19:30:39 +0200

    Do not consider mapping method as a possible candidate method

--- processor/src/main/java/org/mapstruct/ap/internal/processor/creation/MappingResolverImpl.java
+++ processor/src/main/java/org/mapstruct/ap/internal/processor/creation/MappingResolverImpl.java
@@ -196,7 +196,7 @@ public class MappingResolverImpl implements MappingResolver {
 
             this.mappingMethod = mappingMethod;
             this.description = description;
-            this.methods = filterPossibleCandidateMethods( sourceModel );
+            this.methods = filterPossibleCandidateMethods( sourceModel, mappingMethod );
             this.formattingParameters =
                 formattingParameters == null ? FormattingParameters.EMPTY : formattingParameters;
             this.sourceRHS = sourceRHS;
@@ -210,10 +210,10 @@ public class MappingResolverImpl implements MappingResolver {
         }
         // CHECKSTYLE:ON
 
-        private <T extends Method> List<T> filterPossibleCandidateMethods(List<T> candidateMethods) {
+        private <T extends Method> List<T> filterPossibleCandidateMethods(List<T> candidateMethods, T mappingMethod) {
             List<T> result = new ArrayList<>( candidateMethods.size() );
             for ( T candidate : candidateMethods ) {
-                if ( isCandidateForMapping( candidate ) ) {
+                if ( isCandidateForMapping( candidate ) && !candidate.equals( mappingMethod )) {
                     result.add( candidate );
                 }
             }
@@ -229,7 +229,7 @@ public class MappingResolverImpl implements MappingResolver {
             if ( allowMappingMethod() ) {
                 List<SelectedMethod<Method>> matches = getBestMatch( methods, sourceType, targetType );
                 reportErrorWhenAmbiguous( matches, targetType );
-                if ( !matches.isEmpty() && !mappingMethod.equals( first( matches ).getMethod() )) {
+                if ( !matches.isEmpty() ) {
                     assignment = toMethodRef( first( matches ) );
                     assignment.setAssignment( sourceRHS );
                     return assignment;
