65be7726638c231ccdfc13654eee892155872f0e
Ben Zegveld <Ben.Zegveld@gmail.com>
2022-08-29 12:23:33 +0200

    #2947: fix checkstyle

--- processor/src/test/java/org/mapstruct/ap/test/generics/typebounds/WildcardNestedInheritedExtendsMapper.java
+++ processor/src/test/java/org/mapstruct/ap/test/generics/typebounds/WildcardNestedInheritedExtendsMapper.java
@@ -14,7 +14,8 @@ import org.mapstruct.factory.Mappers;
  */
 @Mapper
 public abstract class WildcardNestedInheritedExtendsMapper {
-    public static final WildcardNestedInheritedExtendsMapper INSTANCE = Mappers.getMapper( WildcardNestedInheritedExtendsMapper.class );
+    public static final WildcardNestedInheritedExtendsMapper INSTANCE =
+        Mappers.getMapper( WildcardNestedInheritedExtendsMapper.class );
 
     @Mapping( target = "object", source = "contained.object" )
     public abstract Target map(SourceContainerInherited<?> source);
