5f1c876bf1fa561fa58b0abaffab4ee106c30d9d
Filip Hrisafov <filip.hrisafov@gmail.com>
2022-09-02 19:28:06 +0200

    Add missing license

--- processor/src/test/java/org/mapstruct/ap/test/bugs/_2743/Issue2743Test.java
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_2743/Issue2743Test.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package org.mapstruct.ap.test.bugs._2743;
 
 import org.mapstruct.ap.testutil.IssueKey;
@@ -15,6 +20,5 @@ class Issue2743Test {
 
     @ProcessorTest
     void shouldCompile() {
-
     }
 }
