8c3288b5aaa454318e186900f9e6cef76e5213fa
Filip Hrisafov <filip.hrisafov@gmail.com>
2023-05-01 09:15:53 +0200

    Simplify test

--- processor/src/test/java/org/mapstruct/ap/test/bugs/_3165/Issue3165MapperTest.java
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_3165/Issue3165MapperTest.java
@@ -7,11 +7,11 @@ package org.mapstruct.ap.test.bugs._3165;
 
 import java.util.Arrays;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.mapstruct.ap.testutil.IssueKey;
 import org.mapstruct.ap.testutil.ProcessorTest;
 import org.mapstruct.ap.testutil.WithClasses;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @WithClasses({
     Issue3165Mapper.class
@@ -19,25 +19,14 @@ import org.mapstruct.ap.testutil.WithClasses;
 @IssueKey("3165")
 class Issue3165MapperTest {
 
-    private Issue3165Mapper.Source src;
-
-    @BeforeEach
-    void setUp() {
-        src = new Issue3165Mapper.Source(
+    @ProcessorTest
+    void supportsAdderWhenMappingArrayAndIterableToCollection() {
+        Issue3165Mapper.Source src = new Issue3165Mapper.Source(
             new String[] { "cat", "dog", "mouse" },
             Arrays.asList( "ivy", "flu", "freya" )
         );
-    }
-
-    @ProcessorTest
-    void supportsAdderWhenMappingArrayToCollection() {
         Issue3165Mapper.Target target = Issue3165Mapper.INSTANCE.toTarget( src );
-        Assertions.assertThat( target.getPets() ).containsExactly( "cat", "dog", "mouse" );
-    }
-
-    @ProcessorTest
-    void supportsAdderWhenMappingIterableToCollection() {
-        Issue3165Mapper.Target target = Issue3165Mapper.INSTANCE.toTarget( src );
-        Assertions.assertThat( target.getCats() ).containsExactly( "ivy", "flu", "freya" );
+        assertThat( target.getPets() ).containsExactly( "cat", "dog", "mouse" );
+        assertThat( target.getCats() ).containsExactly( "ivy", "flu", "freya" );
     }
 }
