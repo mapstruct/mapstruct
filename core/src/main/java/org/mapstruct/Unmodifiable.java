261044ffcdc64d660a5f14ba86fef82f1d17565f
Juri Berlanda <juriberlanda@hotmail.com>
2023-08-06 16:19:24 +0200

    #3334 Refine @Unmodifiable

--- core/src/main/java/org/mapstruct/Unmodifiable.java
+++ core/src/main/java/org/mapstruct/Unmodifiable.java
@@ -5,5 +5,13 @@
  */
 package org.mapstruct;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+@Retention( CLASS )
+@Target( { METHOD } )
 public @interface Unmodifiable {
 }
