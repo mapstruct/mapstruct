99e01d7acbf79bc99314f8a4ef85039816b6943a
zyberzebra <zyberzebra@users.noreply.github.com>
2024-05-25 21:41:25 +0200

    adds copyright header

--- processor/src/test/java/org/mapstruct/ap/test/emptytarget/JarMapperTest.java
+++ processor/src/test/java/org/mapstruct/ap/test/emptytarget/JarMapperTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package org.mapstruct.ap.test.emptytarget;
 
 import org.mapstruct.ap.testutil.ProcessorTest;
@@ -5,8 +10,6 @@ import org.mapstruct.ap.testutil.WithClasses;
 import org.mapstruct.ap.testutil.compilation.annotation.CompilationResult;
 import org.mapstruct.ap.testutil.compilation.annotation.Diagnostic;
 import org.mapstruct.ap.testutil.compilation.annotation.ExpectedCompilationOutcome;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 
 class JarMapperTest {
