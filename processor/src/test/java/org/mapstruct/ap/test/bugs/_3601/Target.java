6effaffc9f1cd0db21727d68dce423b49953bd38
Filip Hrisafov <filip.hrisafov@gmail.com>
2024-07-06 10:20:38 +0200

    Add missing copyright headers

--- processor/src/test/java/org/mapstruct/ap/test/bugs/_3601/Target.java
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_3601/Target.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package org.mapstruct.ap.test.bugs._3601;
 
 import java.util.List;
