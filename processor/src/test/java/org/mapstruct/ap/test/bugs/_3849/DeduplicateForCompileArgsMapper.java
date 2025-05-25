0badba70038875cb2fdf6d9ff328b6a9ba31d245
Yang Tang <tangyang9464@163.com>
2025-05-25 22:35:38 +0800

    #3849: Resolve duplicate invocation of overloaded lifecycle methods with inheritance
    Add compiler option `mapstruct.disableLifecycleOverloadDeduplicateSelector` to disable the deduplication if needed.
    
    Signed-off-by: TangYang <tangyang9464@163.com>

---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_3849/DeduplicateForCompileArgsMapper.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.bugs._3849;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mapstruct.AfterMapping;
+import org.mapstruct.BeforeMapping;
+import org.mapstruct.Context;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface DeduplicateForCompileArgsMapper {
+
+    DeduplicateForCompileArgsMapper INSTANCE = Mappers.getMapper( DeduplicateForCompileArgsMapper.class );
+    List<String> INVOKED_METHODS = new ArrayList<>();
+
+    ParentDto mapParent(Parent source, @Context MappingContext context);
+
+    ChildDto mapChild(Parent source, @Context MappingContext context);
+
+    class MappingContext {
+        @BeforeMapping
+        void deduplicateByTargetForBefore(Parent source, @MappingTarget ParentDto target) {
+            INVOKED_METHODS.add( "beforeMappingParentTargetInOtherClass" );
+        }
+
+        @BeforeMapping
+        void deduplicateByTargetForBefore(Parent source, @MappingTarget ChildDto target) {
+            INVOKED_METHODS.add( "beforeMappingChildTargetInOtherClass" );
+        }
+
+        @AfterMapping
+        void deduplicateByTarget(Parent source, @MappingTarget ParentDto target) {
+            INVOKED_METHODS.add( "afterMappingParentTargetInOtherClass" );
+        }
+
+        @AfterMapping
+        void deduplicateByTarget(Parent source, @MappingTarget ChildDto target) {
+            INVOKED_METHODS.add( "afterMappingChildTargetInOtherClass" );
+        }
+    }
+
+    @BeforeMapping
+    default void deduplicateByTargetForBefore(Parent source, @MappingTarget ParentDto target) {
+        INVOKED_METHODS.add( "beforeMappingParentTarget" );
+    }
+
+    @BeforeMapping
+    default void deduplicateByTargetForBefore(Parent source, @MappingTarget ChildDto target) {
+        INVOKED_METHODS.add( "beforeMappingChildTarget" );
+    }
+
+    @AfterMapping
+    default void deduplicateByTarget(Parent source, @MappingTarget ParentDto target) {
+        INVOKED_METHODS.add( "afterMappingParentTarget" );
+    }
+
+    @AfterMapping
+    default void deduplicateByTarget(Parent source, @MappingTarget ChildDto target) {
+        INVOKED_METHODS.add( "afterMappingChildTarget" );
+    }
+}
---
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_3849/ParentDto.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.bugs._3849;
+
+public class ParentDto {
+    private String value;
+
+    public ParentDto(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
