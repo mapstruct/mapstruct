225c268d54c0e7f28c0ac663f914765c8713c748
이현수 <znight1020@naver.com>
2025-07-30 01:02:59 +0900

    build: exclude issue #3902 from full feature test of integration test

--- integrationtest/src/test/java/org/mapstruct/itest/tests/FullFeatureCompilationExclusionCliEnhancer.java
+++ integrationtest/src/test/java/org/mapstruct/itest/tests/FullFeatureCompilationExclusionCliEnhancer.java
@@ -27,6 +27,7 @@ public final class FullFeatureCompilationExclusionCliEnhancer implements Process
         additionalExcludes.add( "org/mapstruct/ap/test/bugs/_1596/*.java" );
         additionalExcludes.add( "org/mapstruct/ap/test/bugs/_1801/*.java" );
         additionalExcludes.add( "org/mapstruct/ap/test/bugs/_3089/*.java" );
+        additionalExcludes.add( "org/mapstruct/ap/test/bugs/_3902/*.java" );
 
         switch ( currentJreVersion ) {
             case JAVA_8:
