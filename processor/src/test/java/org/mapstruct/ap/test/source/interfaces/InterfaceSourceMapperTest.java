b749cc7e92e4675bd8748c1b32f2558014f81eda
Diego Pedregal <43535391+thmasker@users.noreply.github.com>
2025-07-31 18:45:34 +0200

    Adds header and author

--- processor/src/test/java/org/mapstruct/ap/test/source/interfaces/InterfaceSourceMapperTest.java
+++ processor/src/test/java/org/mapstruct/ap/test/source/interfaces/InterfaceSourceMapperTest.java
@@ -1,9 +1,17 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package org.mapstruct.ap.test.source.interfaces;
 
 import org.mapstruct.ap.testutil.IssueKey;
 import org.mapstruct.ap.testutil.ProcessorTest;
 import org.mapstruct.ap.testutil.WithClasses;
 
+/**
+ * @author Diego Pedregal
+ */
 @WithClasses({
     InterfaceMapper.class,
     A.class,
