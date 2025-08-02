1ed6c83fc86e095e3df290c2ac4624faa8c548ca
Gavriil <gavriiltzortzakis@gmail.com>
2025-08-02 15:25:00 +0200

    #3619 Add SPI for implementation naming

---
+++ processor/src/test/java/org/mapstruct/ap/test/naming/spi/ImplementationNamingStrategy/CustomImplementationNamingTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.naming.spi.ImplementationNamingStrategy;
+
+import org.mapstruct.ap.testutil.ProcessorTest;
+import org.mapstruct.ap.testutil.WithClasses;
+import org.mapstruct.ap.testutil.WithServiceImplementation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WithClasses( { MyObject.class, MyObjectDto.class, MyObjectMapper.class } )
+@WithServiceImplementation( CustomImplementationNamingStrategy.class )
+public class CustomImplementationNamingTest {
+    @ProcessorTest
+    public void shouldGenerateCustomNameForImplementation() {
+        MyObjectMapper instance = MyObjectMapper.INSTANCE;
+        MyObject myObject = new MyObject( "Gavriil" );
+        MyObjectDto myObjectDto = instance.toDto( myObject );
+
+        assertThat( instance ).isInstanceOf( MyObjectMapper.class );
+        String expectedName = new CustomImplementationNamingStrategy()
+                .generateImplementationName( MyObjectMapper.class.getSimpleName(), null );
+        assertThat( instance.getClass().getSimpleName() ).isEqualTo( expectedName );
+        assertThat( myObjectDto.getName() ).isEqualTo( myObject.getName() );
+
+    }
+}
+
---
+++ processor/src/test/java/org/mapstruct/ap/test/naming/spi/ImplementationNamingStrategy/MyObjectMapper.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.test.naming.spi.ImplementationNamingStrategy;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface MyObjectMapper {
+    MyObjectMapper INSTANCE =
+            Mappers.getMapper( MyObjectMapper.class );
+
+    MyObjectDto toDto(MyObject myObject);
+}
