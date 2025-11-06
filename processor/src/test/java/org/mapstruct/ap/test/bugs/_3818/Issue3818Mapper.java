117eb8baab71785b90ef8b90d2b75f88940b3fcf
Filip Gala <filip.gala@telekom.com>
2025-11-06 12:43:39 +0100

    added missing header

--- processor/src/test/java/org/mapstruct/ap/test/bugs/_3818/Issue3818Mapper.java
+++ processor/src/test/java/org/mapstruct/ap/test/bugs/_3818/Issue3818Mapper.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package org.mapstruct.ap.test.bugs._3818;
 
 import org.mapstruct.Mapper;
