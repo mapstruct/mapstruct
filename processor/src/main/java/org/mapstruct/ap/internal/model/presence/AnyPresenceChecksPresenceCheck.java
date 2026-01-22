8ff510695c766c70571ea70c327a80ff1e0e7dee
Filip Hrisafov <filip.hrisafov@gmail.com>
2026-01-22 23:59:34 +0100

    Simplify NestedPropertyMappingMethod.ftl by moving logic into java instead of freemarker

---
+++ processor/src/main/java/org/mapstruct/ap/internal/model/presence/AnyPresenceChecksPresenceCheck.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.mapstruct.ap.internal.model.presence;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import org.mapstruct.ap.internal.model.common.ModelElement;
+import org.mapstruct.ap.internal.model.common.NegatePresenceCheck;
+import org.mapstruct.ap.internal.model.common.PresenceCheck;
+import org.mapstruct.ap.internal.model.common.Type;
+
+/**
+ * @author Filip Hrisafov
+ */
+public class AnyPresenceChecksPresenceCheck extends ModelElement implements PresenceCheck {
+
+    private final Collection<PresenceCheck> presenceChecks;
+
+    public AnyPresenceChecksPresenceCheck(Collection<PresenceCheck> presenceChecks) {
+        this.presenceChecks = presenceChecks;
+    }
+
+    public Collection<PresenceCheck> getPresenceChecks() {
+        return presenceChecks;
+    }
+
+    @Override
+    public PresenceCheck negate() {
+        return new NegatePresenceCheck( this );
+    }
+
+    @Override
+    public Set<Type> getImportTypes() {
+        Set<Type> importTypes = new HashSet<>();
+        for ( PresenceCheck presenceCheck : presenceChecks ) {
+            importTypes.addAll( presenceCheck.getImportTypes() );
+        }
+
+        return importTypes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if ( this == o ) {
+            return true;
+        }
+        if ( o == null || getClass() != o.getClass() ) {
+            return false;
+        }
+        AnyPresenceChecksPresenceCheck that = (AnyPresenceChecksPresenceCheck) o;
+        return Objects.equals( presenceChecks, that.presenceChecks );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash( presenceChecks );
+    }
+}
