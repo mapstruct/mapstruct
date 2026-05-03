f492c3bc8481d44a53cd512eee609080589f0ec2
takch02 <lim70kr38@gmail.com>
2026-05-03 13:52:18 +0900

    #4027 Add fixture for SourceTargetMapperImpl

---
+++ processor/src/test/resources/fixtures/org/mapstruct/ap/test/conversion/java8time/zonedoffsetdatetimetolocaldatetimeconversion/SourceTargetMapperImpl.java
@@ -0,0 +1,29 @@
+package org.mapstruct.ap.test.conversion.java8time.zonedoffsetdatetimetolocaldatetimeconversion;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2026-05-03T13:46:42+0900",
+    comments = "version: , compiler: javac, environment: Java 25.0.2 (Homebrew)"
+)
+public class SourceTargetMapperImpl implements SourceTargetMapper {
+
+    @Override
+    public Target toTarget(Source source) {
+        if ( source == null ) {
+            return null;
+        }
+
+        Target target = new Target();
+
+        if ( source.getOffsetDateTime() != null ) {
+            target.setOffsetDateTime( source.getOffsetDateTime().toLocalDateTime() );
+        }
+        if ( source.getZonedDateTime() != null ) {
+            target.setZonedDateTime( source.getZonedDateTime().toLocalDateTime() );
+        }
+
+        return target;
+    }
+}
