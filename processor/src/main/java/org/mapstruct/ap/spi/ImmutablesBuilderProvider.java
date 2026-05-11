6d0a991850b8b8dce946a84fb4a89d7efc537a34
Thomas Humper <thomashumper69@gmail.com>
2026-05-11 19:35:58 +0200

    Update ImmutablesBuilderProvider.java

--- processor/src/main/java/org/mapstruct/ap/spi/ImmutablesBuilderProvider.java
+++ processor/src/main/java/org/mapstruct/ap/spi/ImmutablesBuilderProvider.java
@@ -4,14 +4,9 @@
  * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 package org.mapstruct.ap.spi;
-
-import java.util.regex.Pattern;
-import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
-import javax.lang.model.element.Name;
-import javax.lang.model.element.PackageElement;
-import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
 
 import org.mapstruct.util.Experimental;
 
@@ -30,71 +25,58 @@ public class ImmutablesBuilderProvider extends DefaultBuilderProvider {
     private static final String IMMUTABLE_FQN = "org.immutables.value.Value.Immutable";
 
     @Override
-    protected BuilderInfo findBuilderInfo(TypeElement typeElement) {
-        Name name = typeElement.getQualifiedName();
-        if ( name.length() == 0 || JAVA_JAVAX_PACKAGE.matcher( name ).matches() ) {
-            return null;
-        }
-
-        // First look if there is a builder defined in my own type
-        BuilderInfo info = findBuilderInfo( typeElement, false );
-        if ( info != null ) {
-            return info;
-        }
-
-        // Check for a builder in the generated immutable type
-        BuilderInfo immutableInfo = findBuilderInfoForImmutables( typeElement );
-        if ( immutableInfo != null ) {
-            return immutableInfo;
-        }
-
-        return super.findBuilderInfo( typeElement.getSuperclass() );
-    }
-
-    protected BuilderInfo findBuilderInfoForImmutables(TypeElement typeElement) {
-        TypeElement immutableAnnotation = elementUtils.getTypeElement( IMMUTABLE_FQN );
-        if ( immutableAnnotation != null ) {
-            return findBuilderInfoForImmutables(
-                typeElement,
-                immutableAnnotation
-            );
-        }
+protected BuilderInfo findBuilderInfo(TypeElement typeElement) {
+    Name name = typeElement.getQualifiedName();
+    if (name.length() == 0 || JAVA_JAVAX_PACKAGE.matcher(name).matches()) {
         return null;
     }
 
-    protected BuilderInfo findBuilderInfoForImmutables(TypeElement typeElement,
-                                                       TypeElement immutableAnnotation) {
-        for ( AnnotationMirror annotationMirror : elementUtils.getAllAnnotationMirrors( typeElement ) ) {
-            if ( typeUtils.isSameType( annotationMirror.getAnnotationType(), immutableAnnotation.asType() ) ) {
-                TypeElement immutableElement = asImmutableElement( typeElement );
-                if ( immutableElement != null ) {
-                    return super.findBuilderInfo( immutableElement, false );
-                }
-                else {
-                    // Immutables processor has not run yet. Trigger a postpone to the next round for MapStruct
-                    throw new TypeHierarchyErroneousException( typeElement );
-                }
-            }
-        }
+    // First look if there is a builder defined in my own type
+    BuilderInfo info = findBuilderInfo(typeElement, false);
+    if (info != null) {
+        return info;
+    }
+
+    // Check for a builder in the generated immutable type
+    BuilderInfo immutableInfo = findBuilderInfoForImmutables(typeElement);
+    if (immutableInfo != null) {
+        return immutableInfo;
+    }
+
+    // FIX: superclass is TypeMirror, not TypeElement
+    TypeMirror superclass = typeElement.getSuperclass();
+    if (superclass.getKind() == TypeKind.NONE) {
         return null;
     }
 
-    protected TypeElement asImmutableElement(TypeElement typeElement) {
-        Element enclosingElement = typeElement.getEnclosingElement();
-        StringBuilder builderQualifiedName = new StringBuilder( typeElement.getQualifiedName().length() + 17 );
-        if ( enclosingElement.getKind() == ElementKind.PACKAGE ) {
-            builderQualifiedName.append( ( (PackageElement) enclosingElement ).getQualifiedName().toString() );
+    if (superclass instanceof DeclaredType declaredType) {
+        Element superElement = declaredType.asElement();
+        if (superElement instanceof TypeElement superTypeElement) {
+            return super.findBuilderInfo(superTypeElement);
         }
-        else {
-            builderQualifiedName.append( ( (TypeElement) enclosingElement ).getQualifiedName().toString() );
-        }
-
-        if ( builderQualifiedName.length() > 0 ) {
-            builderQualifiedName.append( "." );
-        }
-
-        builderQualifiedName.append( "Immutable" ).append( typeElement.getSimpleName() );
-        return elementUtils.getTypeElement( builderQualifiedName );
     }
+
+    return null;
+}
+protected TypeElement asImmutableElement(TypeElement typeElement) {
+    StringBuilder fqName = new StringBuilder();
+
+    Element current = typeElement.getEnclosingElement();
+
+    // FIX: support nested classes properly
+    while (current != null && current.getKind() != ElementKind.PACKAGE) {
+        fqName.insert(0, ((TypeElement) current).getSimpleName() + "$");
+        current = current.getEnclosingElement();
+    }
+
+    if (current instanceof PackageElement pkg) {
+        fqName.insert(0, pkg.getQualifiedName().toString() + ".");
+    }
+
+    fqName.append("Immutable").append(typeElement.getSimpleName());
+
+    // FIX: must pass String
+    return elementUtils.getTypeElement(fqName.toString());
+}
 
 }
